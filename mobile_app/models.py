78451c49fed575219b2ce22c0c804bb7dbfe7ced
Nguyễn Anh Trọng <92981684+anhtrong-kms@users.noreply.github.com>
2024-04-14 09:31:48 +0700

    update

--- mobile_app/models.py
+++ mobile_app/models.py
@@ -17,7 +17,6 @@ class Customer(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
 
     def __str__(self):
         return self.name
--- mobile_app/urls.py
+++ mobile_app/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import update_products_from_api
 
 urlpatterns = [
     path('', views.Store, name="Store"),
