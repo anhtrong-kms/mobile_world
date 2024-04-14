3773f6628021dad1cf2e654b32c4b1999c3f10c5
Nguyễn Anh Trọng <92981684+anhtrong-kms@users.noreply.github.com>
2024-04-14 09:26:42 +0700

    update

--- mobile_app/urls.py
+++ mobile_app/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', views.Store, name="Store"),
     path('login/', views.Login, name="Login"),
     path('register/', views.Register, name="Register"),
-    path('update-products/', update_products_from_api, name='update_products'),
 ]
--- mobile_app/views.py
+++ mobile_app/views.py
@@ -12,33 +12,3 @@ def Login(request):
 
 def Register(request):
     return render(request, 'register.html', {})
-
-def update_products_from_api(request):
-    # Gửi yêu cầu GET đến API và lấy dữ liệu
-    response = requests.get('https://fakestoreapi.com/products')
-    
-    if response.status_code == 200:
-        products_data = response.json()  # Chuyển đổi dữ liệu nhận được sang định dạng JSON
-        for product_data in products_data:
-            # Tạo hoặc cập nhật sản phẩm trong cơ sở dữ liệu Django từ dữ liệu nhận được từ API
-            product, created = Product.objects.update_or_create(
-                id=product_data['id'],
-                defaults={
-                    'name': product_data['title'],
-                    'description': product_data['description'],
-                    'price': product_data['price'],
-                    'category': product_data['category'],
-                    'image': product_data['image'],
-                    # Thêm các trường khác của model Product nếu cần
-                }
-            )
-            # Update sold_quantity và remaining_quantity tương ứng
-            product.remaining_quantity = product_data.get('quantity', 0)
-            product.sold_quantity = product_data.get('quantity', 0) - product.remaining_quantity
-            product.save()
-            
-        message = "Dữ liệu đã được cập nhật từ API thành công."
-    else:
-        message = "Không thể lấy dữ liệu từ API."
-    
-    return HttpResponse(message)
