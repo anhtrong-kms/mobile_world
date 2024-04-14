2d1f0bd11c7e37f58aaa701b930e6903388a6ba4
thanhphat365 <thanhphat.nguyen2008@gmail.com>
2024-04-14 08:37:13 +0700

    models update

--- mobile_app/models.py
+++ mobile_app/models.py
@@ -13,11 +13,27 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
-class Product(models.Model):  
-    name = models.CharField(max_length=500)
-    price = models.FloatField(default=15.5)
-    image = models.ImageField()  
+class Product(models.Model):
+    image = models.ImageField(null=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    sold_quantity = models.IntegerField(default=0)
+    remaining_quantity = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    origin = models.CharField(max_length=100)
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
 
+    def discounted_price(self):
+        discount_amount = Decimal(self.price) * Decimal(self.discount) / Decimal(100)
+        discounted_price = Decimal(self.price) - discount_amount
+        return discounted_price
+    def sold_quatily_sp(self):
+        sold_qua = self.sold_quantity+1
+        return  sold_qua
+    def remain_quatily(self):
+        remain_qua = self.remaining_quantity-1
+        return remain_qua
     def __str__(self):
         return self.name
 
