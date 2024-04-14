f3e4eab5ddbcffe06a47698fb86e192a984a741a
Nguyễn Anh Trọng <92981684+anhtrong-kms@users.noreply.github.com>
2024-04-14 09:19:12 +0700

    update view
    get data api

--- mobile_app/urls.py
+++ mobile_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import update_products_from_api
 
 urlpatterns = [
     path('', views.Store, name="Store"),
     path('login/', views.Login, name="Login"),
     path('register/', views.Register, name="Register"),
+    path('update-products/', update_products_from_api, name='update_products'),
 ]
