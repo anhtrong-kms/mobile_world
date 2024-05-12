38e823f2531cc0f20a4cd36ace6c362efc22ec54
anhtrong-kms <2001210175@hufi.edu.vn>
2024-05-12 08:34:36 +0700

    Update Urls.py
    Hiển thị hình ảnh

--- mobile_app/urls.py
+++ mobile_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.Store, name="Store"),
     path('login/', views.Login, name="Login"),
     path('register/', views.Register, name="Register"),
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
