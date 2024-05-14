415e188320b11e0d4e5bf4b13b3908fefde73192
thanhphat365 <thanhphat.nguyen2008@gmail.com>
2024-05-14 23:06:12 +0700

    login update

---
+++ static/js/scrip.js
@@ -0,0 +1,23 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');  
+
+// click=> add|remove   
+
+registerBtn.addEventListener('click', () =>{ 
+  container.classList.add("active");
+});
+loginBtn.addEventListener('click', () =>{ 
+  container.classList.remove("active");
+}); 
+
+// const btnShowPass = document.querySelector("#show-pass");
+// const inputPass = document.querySelector("#input-pass");
+// btnShowPass.addEventListener('click', function(){
+//     btnShowPass.classList.toggle("active");
+//     if(inputPass.type === "password"){
+//         inputPass.type = "text";
+//     }else {
+//         inputPass.type = "password";
+//     }
+// });
